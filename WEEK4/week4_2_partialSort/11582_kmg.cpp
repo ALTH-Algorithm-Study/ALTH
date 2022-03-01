de0990d05cafb7265f2c186d6bba184ab03bce66
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-03-01 19:13:52 +0900

    [김민겸] 0301 19:13 update
    11582 성공 코드

--- WEEK4/week4_2_partialSort/11582_kmg.cpp
+++ WEEK4/week4_2_partialSort/11582_kmg.cpp
@@ -4,85 +4,45 @@
 using namespace std;
 #define endl '\n'
 
-int n; // 치킨집의 개수
-int* arr; // N개의 치킨집의 맛의 수치
-int k; // 현재 정렬을 진행 중인 회원들의 수 
+int n;
+int* arr;
+int k;
+int* tmp;
 
-/*
-* 의사코드
-*
-* for(i는 0부터 log2K까지 순회)
-*	while(end != n) {
-*		int start = 0;
-		int end = pow(2, i);
-		sort(start, end);
-		start += end;
-		end += end;
-		if(i == log_2_k(k))
-			break;
-	}
-*
-*/
+void merge(int left, int right) {
+	if (right - left > n / k) return;
 
-int log_2_K(int k) {
-	return log(k) / log(2);
-}
+	int l, r, k, a; // 왼쪽 배열의 인덱스
+	int mid = (left + right) / 2;
+	l = left; // 왼쪽 배열의 첫 인덱스
+	r = mid + 1; // 오른쪽 배열의 첫 인덱스
+	k = left; // tmp라는 임시 배열의 인덱스
 
-/*
-
-auto partition(int start, int end) {
-	int pivot = arr[start]; // pivot을 배열의 가장 왼쪽으로 선정
-	int left = start + 1;
-	int right = end;
-
-	while (true) {
-		while (arr[left] <= pivot && right - left > 0)
-			left++;
-		while (arr[right] > pivot && right - left > 0)
-			right--;
-
-		if (left == right)
-			break;
-		else
-			swap(arr[left], arr[right]);
+	while (l <= mid && r <= right) {
+		tmp[k++] = arr[l] <= arr[r] ? arr[l++] : arr[r++];
 	}
 
-	if (pivot > arr[right])
-		swap(arr[start], arr[right]);
+	while (l <= mid) tmp[k++] = arr[l++];// 오른쪽 배열의 요소가 남았을 경우
+	while (r <= right) tmp[k++] = arr[r++];// 왼쪽 배열의 요소가 남았을 경우
 
-	return right; // 오른쪽 부분 배열의 시작 지점을 반환
-}
-
-void quickSort(int start, int end) {
-	if (end - start >= 1) {
-		int iter = partition(start, end);
-
-		quickSort(start, iter - 1);
-		quickSort(iter, end);
+	for (int i = left; i <= right; i++) {
+		arr[i] = tmp[i]; // 변경된 tmp배열을 arr배열에 복사해준다. 
 	}
 
 }
-*/
 
+void mergeSort(int left, int right) {
+	if (left == right) return;
+
+	int mid = (left + right) / 2;
+	mergeSort(left, mid);
+	mergeSort(mid + 1, right);
+
+	merge(left, right);
+}
 
 void sol() {
-	for (int i = log_2_K(n) - 1; i >= 0; i--) {  // i는 2, 1, 0 이렇게 역순으로 순회한다. 
-		int gap = pow(2, log_2_K(n) - i);
-		int start = 0;
-		int end = gap;
-		while (end <= n) { // end가 n을 초과해서 수행해야할 정렬 범위를 넘었을 경우 
-			sort(arr + start, arr + end);
-			start += gap; // 현재 start지점에서 end를 더해 
-			end += gap; // 현재 end 지점에서 i = 2, 1, 0일 때 1, 2, 4를 더한다. 
-			if (i == log_2_K(k)) { // 정렬을 진행중인 회원 수가 2의 i승과 일치할 때
-				sort(arr + start, arr + end); // break 하기 전 마지막으로 저장된 start, end에 대한 정렬을 수행한다. 
-				break;
-			}
-		}
-		if (i == log_2_K(k))
-			break;
-	}
-
+	mergeSort(0, n - 1);
 	for (int i = 0; i < n; i++) {
 		cout << arr[i] << " ";
 	}
@@ -90,12 +50,13 @@ void sol() {
 }
 
 int main() {
-	ios_base::sync_with_stdio(false) ;
+	ios_base::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
 
 	cin >> n;
 	arr = new int[n];
+	tmp = new int[n];
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
