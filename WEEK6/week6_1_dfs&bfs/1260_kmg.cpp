519f449693f0dd373e7322d15f9e1461f339a2f9
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-03-14 15:12:32 +0900

    [김민겸] 0314 15:13 update

--- WEEK6/week6_1_dfs&bfs/1260_kmg.cpp
+++ WEEK6/week6_1_dfs&bfs/1260_kmg.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <queue>
 #define endl '\n'
+#define MAX 1001
 using namespace std;
 
 int n; // 정점의 개수
 int m; // 간선의 개수
 int v; // 탐색을 시작한 정점의 번호
-bool visitBFS[1001];
-bool visitDFS[1001];
-vector<int> graph[1001];
+bool visitBFS[MAX];
+bool visitDFS[MAX];
+bool graph[MAX][MAX];
 
 void bfs(int start) {
 	queue<int> q;
@@ -19,12 +20,11 @@ void bfs(int start) {
 		int x = q.front();
 		q.pop();
 		cout << x << " "; // 순회하는 노드의 값을 보여줌. 
-		for (int i = 0; i < graph[x].size(); i++) {
-			int y = graph[x][i];
-			if (!visitBFS[y]) { // 방문하지 않은 노드라면
-				q.push(y); // 인접 노드를 큐에 넣고
-				visitBFS[y] = true; // 방문 처리한다.
-			}
+		for (int i = 1; i <= n; i++) {
+			if (visitBFS[i] == 1 || graph[x][i] == 0) // 방문한 노드이거나 인접한 노드가 아닌 경우
+				continue; // PASS
+			q.push(i);
+			visitBFS[i] = true;
 		}
 	}
 }
@@ -32,11 +32,10 @@ void bfs(int start) {
 void dfs(int start) {
 	visitDFS[start] = true;
 	cout << start << " ";
-	for (int i = 0; i < graph[start].size(); i++) {
-		int y = graph[start][i];
-		if (!visitDFS[y]) {
-			dfs(y);
-		}
+	for (int i = 1; i <= n; i++) {
+		if (visitDFS[i] == 1 || graph[start][i] == 0) // 방문한 노드이거나 인접한 노드가 아닌 경우
+			continue;
+		dfs(i);
 	}
 }
 
@@ -52,8 +51,7 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int tmp1, tmp2;
 		cin >> tmp1 >> tmp2;
-		graph[tmp1].push_back(tmp2);
-		graph[tmp2].push_back(tmp1);
+		graph[tmp1][tmp2] = graph[tmp2][tmp1] = 1;
 	}
 
 	dfs(v);
